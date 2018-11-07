fffc6f53f6ddba364034f78c57aa67cc22812ea0
Avi <44028161+avikor@users.noreply.github.com>
2018-11-07 19:15:55 +0200

    regularized

--- csp/forward_checking_implementation.py
+++ csp/forward_checking_implementation.py
@@ -2,9 +2,9 @@ from csp.variable import Variable
 from csp.constraint_problem import ConstraintProblem
 
 
-def forward_check(constraints_problem: ConstraintProblem, assigned_variable: Variable) -> bool:
-    unassigned_neighbors_frozenset = constraints_problem.get_unassigned_neighbors(assigned_variable)
+def forward_check(constraint_problem: ConstraintProblem, assigned_variable: Variable) -> bool:
+    unassigned_neighbors_frozenset = constraint_problem.get_unassigned_neighbors(assigned_variable)
     unsatisfiable_neighbors = filter(lambda unassigned_neighbor:
-                                     not constraints_problem.get_consistent_domain(unassigned_neighbor),
+                                     not constraint_problem.get_consistent_domain(unassigned_neighbor),
                                      unassigned_neighbors_frozenset)
     return False if any(unsatisfiable_neighbors) else True
